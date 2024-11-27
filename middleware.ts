908cc9312e99c9053edf04aefcca7e2eaf22a70d
jjcxdev <justinjchambers@gmail.com>
2024-11-27 16:40:16 -0500

    fixed middleware.ts type on routine

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/exercise(.*)', '/routine(.*'])
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/exercise(.*)', '/routine(.*)'])
 
 export default clerkMiddleware(async (auth, req)=> {
     if (isProtectedRoute(req)) await auth.protect()
