dcecd8779b024947a5cbea99afe2308fb7515375
jjcxdev <justinjchambers@gmail.com>
2024-11-28 01:41:50 -0500

    modified ExerciseCard with color change onclick

--- app/components/ExerciseCard.tsx
+++ app/components/ExerciseCard.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 interface ExerciseCardProps {
   title: string;
@@ -7,13 +9,27 @@ interface ExerciseCardProps {
 }
 
 export default function ExerciseCard({ ...props }: ExerciseCardProps) {
+  const [borderColor, setBorderColor] = useState("border-border");
+  const [borderWidth, setBorderWidth] = useState("border");
+
+  function handleColorChange(): void {
+    setBorderColor((prev) =>
+      prev === "border-border" ? "border-accent" : "border-border",
+    );
+    setBorderWidth((prev) => (prev === "border" ? "border-4" : "border"));
+  }
+
   return (
-    <div className="rounded-lg border border-border p-4">
-      <div className="flex justify-between">
-        <div className="font-bold">{props.title}</div>
-        <div className="text-sm italic text-accent">{props.exerciseType}</div>
+    <button onClick={handleColorChange} className="h-fit w-full">
+      <div className={`rounded-lg border ${borderColor} ${borderWidth} p-4`}>
+        <div className="flex justify-between">
+          <div className="font-bold">{props.title}</div>
+          <div className="text-sm italic text-accent">{props.exerciseType}</div>
+        </div>
+        <div className="flex w-full justify-start text-sm text-dimmed">
+          {props.muscleGroup}
+        </div>
       </div>
-      <div className="text-sm text-dimmed">{props.muscleGroup}</div>
-    </div>
+    </button>
   );
 }
