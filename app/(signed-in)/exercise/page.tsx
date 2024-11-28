5a43f0806c0c09e486e1d0ed65867958fe52cdc5
jjcxdev <justinjchambers@gmail.com>
2024-11-28 02:11:04 -0500

    wired up exercises to db - now renders a map of all exercises

--- app/(signed-in)/exercise/page.tsx
+++ app/(signed-in)/exercise/page.tsx
@@ -1,17 +1,35 @@
 import ActionHeader from "@/app/components/ActionHeader";
 import ExerciseCard from "@/app/components/ExerciseCard";
+import SecondaryButton from "@/app/components/SecondaryButton";
+import { db } from "@/lib/db";
+import { asc } from "drizzle-orm";
+import { exercises } from "@/lib/db/schema";
 
-export default function Exercise() {
+export default async function Exercise() {
+  const data = await db
+    .select()
+    .from(exercises)
+    .orderBy((exercises) => [asc(exercises.type), asc(exercises.name)]);
+
   return (
     <div className="flex min-h-screen w-96 flex-col gap-4 border border-red-500">
       <ActionHeader title={"Add Exercise"} button={"Cancel"} />
       <div>
         <div className="p-4">
-          <ExerciseCard
-            title="Lat Pulldown"
-            muscleGroup="Back"
-            exerciseType="Cable"
-          />
+          <ul>
+            {data.map((data) => (
+              <li className="pb-4" key={data.id}>
+                <ExerciseCard
+                  title={data.name}
+                  muscleGroup={data.muscleGroup}
+                  exerciseType={data.type}
+                />
+              </li>
+            ))}
+          </ul>
+          <div className="pt-8">
+            <SecondaryButton label={`Add exercise`} />
+          </div>
         </div>
       </div>
     </div>
