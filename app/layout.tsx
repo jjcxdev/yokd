277d5f6915da2b163df161da68c8d6b4e02b3f46
jjcx <112969641+jjcxdev@users.noreply.github.com>
2025-01-03 16:47:43 -0500

    updated ClerkProvider aftersignouturl (#65)

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
+    <ClerkProvider afterSignOutUrl={'/'} appearance={{ baseTheme: dark }}>
       <html lang="en">
         <body className={`${inter.className} ${helvObl.variable} antialiased`}>
           {children}
