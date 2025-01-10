24274735ca744199539a5ec52a8156ac26c51184
jjcx <112969641+jjcxdev@users.noreply.github.com>
2025-01-10 16:48:24 -0500

    updated SelectFolderModal to shadcn also adjused global button styles

--- app/components/SelectFolderModal.tsx
+++ app/components/SelectFolderModal.tsx
@@ -6,6 +6,8 @@ import React from "react";
 import { FaRegFolderOpen } from "react-icons/fa";
 
 import type { Folders } from "@/types/types";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 interface SelectFolderModalProps {
   isOpen: boolean;
@@ -28,41 +30,41 @@ export default function SelectFolderModal({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0">
-      <div className="absolute inset-0 min-h-screen bg-black/50 blur-sm backdrop-blur-sm"></div>
-      <div className="relative z-50 flex min-h-screen w-full flex-col items-center justify-center">
-        <div className="flex w-full max-w-96 flex-col gap-1 rounded-lg border border-accent/20 bg-card p-4">
-          <div>
-            <h2 className="flex justify-center pb-2 font-bold">
-              Select Program
-            </h2>
-          </div>
-          <div>
-            <ul>
-              {folders.map((folder) => (
-                <li
-                  className="cursor-pointer pb-4"
-                  key={folder.id}
-                  onClick={() => handleFolderSelect(folder.id)}
-                >
-                  <div className="flex items-center gap-4">
-                    <FaRegFolderOpen className="h-4 w-4 text-accent" />
-                    <span>{folder.name}</span>
-                  </div>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-4 flex justify-center">
-              <button
-                onClick={onClose}
-                className="rounded-lg bg-accent/10 px-4 py-2 transition-colors hover:bg-accent/20"
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
+    <>
+      <div className="fixed inset-0">
+        <div className="absolute inset-0 min-h-screen bg-black/50 blur-sm backdrop-blur-sm"></div>
+        <div className="relative z-50 flex min-h-screen w-full flex-col items-center justify-center">
+          <Card className="w-full max-w-96">
+            <CardHeader>
+              <CardTitle className="flex w-full justify-center">
+                Select Program
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <ul>
+                {folders.map((folder) => (
+                  <li
+                    className="cursor-pointer pb-4"
+                    key={folder.id}
+                    onClick={() => handleFolderSelect(folder.id)}
+                  >
+                    <div className="flex items-center gap-4">
+                      <FaRegFolderOpen className="h-4 w-4 text-accent" />
+                      <span>{folder.name}</span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <div className="flex w-full justify-center">
+                {" "}
+                <Button variant="secondary" onClick={onClose}>
+                  Cancel
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-secondary shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-accent/30",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
