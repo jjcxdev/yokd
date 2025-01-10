6d54c06403322ca1f001d5c7e77bb853ce6dd5d6
jjcx <112969641+jjcxdev@users.noreply.github.com>
2025-01-10 02:16:50 -0500

    Forgot to pass the onclick function to the button

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,17 +1,16 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import React from "react";
 import { BsCollectionFill } from "react-icons/bs";
 import { FaDumbbell } from "react-icons/fa6";
 import Image from "next/image";
 import YokdLogo from "@/public/favicon.svg";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 interface EmptyStateProps {
   onCreateFolder: () => void;
@@ -25,17 +24,16 @@ export default function EmptyState({
   return (
     <>
       <Card className="h-min-screen bg-gradient-to-tr from-[#1A1A1A] via-[#1A1A1A] to-[#E7FC00]/20">
-        <CardHeader>
-          <CardTitle className="flex w-full flex-col items-center justify-center gap-2">
-            <Image
-              className="rounded-lg"
-              src={YokdLogo}
-              alt="YOKD Logo"
-              width={60}
-              height={60}
-            />
-            Welcome to YOKD
-          </CardTitle>
+        <CardHeader className="flex flex-col items-center justify-center gap-4">
+          <Image
+            className="rounded-lg"
+            src={YokdLogo}
+            alt="YOKD Logo"
+            width={60}
+            height={60}
+          />
+
+          <CardTitle>Welcome to YOKD</CardTitle>
           <CardDescription className="flex w-full justify-center text-xs">
             Let&apos;s get you started
           </CardDescription>
@@ -45,7 +43,7 @@ export default function EmptyState({
           <p className="text-center text-xs text-dimmed">
             Start by creating a program to organize your routines
           </p>
-          <Button>
+          <Button onClick={onCreateFolder}>
             <BsCollectionFill />
             New Program
           </Button>
@@ -55,7 +53,7 @@ export default function EmptyState({
           <p className="text-center text-xs text-dimmed">
             Create your first workout routine with exercises
           </p>
-          <Button>
+          <Button onClick={onCreateRoutine}>
             <FaDumbbell />
             New Routine
           </Button>
