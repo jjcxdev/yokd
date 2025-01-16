f2222b79dd61194fb0eb3a144df3e96cf7aaf2c6
jjcx <112969641+jjcxdev@users.noreply.github.com>
2025-01-15 22:02:17 -0500

    adjusted SidebarTrigger positioning

--- app/(signed-in)/layout.tsx
+++ app/(signed-in)/layout.tsx
@@ -13,8 +13,9 @@ export default function DashboardLayout({
     <section className="flex min-h-screen w-full justify-center bg-[radial-gradient(circle_at_top,_var(--tw-gradient-stops))] from-background to-card text-white md:px-10">
       <SidebarProvider>
         <AppSidebar />
-
-        <SidebarTrigger />
+        <div className="hidden md:flex md:h-screen md:items-center">
+          <SidebarTrigger />
+        </div>
         {children}
         {/* Mobile Footer section */}
         <footer className="fixed bottom-0 w-full bg-background p-4 text-dimmed md:hidden">
