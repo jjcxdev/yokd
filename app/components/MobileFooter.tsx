026020eee86388251cf0fba7b611998525bee955
jjcx <112969641+jjcxdev@users.noreply.github.com>
2025-01-15 22:26:43 -0500

    93 add side menu to desktop view (#133)
    * added sidebar to layout
    also addede coming soon pages for many others
    
    * adjusted SidebarTrigger positioning
    
    * added SidebarToggle to MobileFooter

--- app/components/MobileFooter.tsx
+++ app/components/MobileFooter.tsx
@@ -7,6 +7,7 @@ import { FaDumbbell } from "react-icons/fa6";
 import { MdDashboard } from "react-icons/md";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 const ClientUserButton = dynamic(
   () => import("@clerk/nextjs").then((mod) => mod.UserButton),
@@ -19,12 +20,14 @@ const ClientUserButton = dynamic(
 export function MobileFooter() {
   return (
     <div className="flex w-full items-center justify-around pb-2">
+      <SidebarTrigger />
       <Link
         href="/"
         className="rounded-full p-[10px] text-2xl focus:bg-accent focus:text-background"
       >
         <MdDashboard />
       </Link>
+
       {/* <button className="rounded-full p-[10px] text-2xl focus:bg-accent focus:text-background">
         <FaDumbbell />
       </button> */}
