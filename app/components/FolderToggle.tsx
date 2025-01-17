59ec904044d5b627ad407ac5043e2d130c5b171a
jjcx <112969641+jjcxdev@users.noreply.github.com>
2025-01-17 17:30:57 -0500

    added animation to folder toggle

--- app/components/FolderToggle.tsx
+++ app/components/FolderToggle.tsx
@@ -31,7 +31,7 @@ export default function FolderToggle({
             className="flex items-center gap-2"
           >
             <BiChevronRight
-              className={`h-4 w-4 transition-transform duration-200 md:h-8 md:w-8 ${isExpanded ? "rotate-90" : ""}`}
+              className={`h-4 w-4 transition-transform duration-200 ease-in-out md:h-8 md:w-8 ${isExpanded ? "rotate-90" : ""}`}
             />
           </Button>
           <div className="flex w-full items-center justify-between">
@@ -44,11 +44,15 @@ export default function FolderToggle({
           </div>
         </CardContent>
 
-        {isExpanded && (
-          <CardFooter className="flex w-full flex-col items-center justify-center">
+        <div
+          className={`overflow-hidden transition-all duration-200 ease-in-out ${isExpanded ? "max-h-96" : "max-h-0"}`}
+        >
+          <CardFooter
+            className={`flex w-full flex-col items-center justify-center transition-all duration-200 ease-in-out ${isExpanded ? "translate-y-0 opacity-100" : "-translate-y-0 opacity-0"}`}
+          >
             <div className="w-full">{children}</div>
           </CardFooter>
-        )}
+        </div>
       </Card>
     </>
   );
